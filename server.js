bd071c9fbf6eeca6a1cf2cbbd5c0e8f1fa9ece45
tornicke <torniqe@gmail.com>
2022-10-08 10:38:27 +0200

    Created basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});

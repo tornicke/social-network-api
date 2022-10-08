9f619358e5eb09b8dc2f4db7caa7ee769674d13a
tornicke <torniqe@gmail.com>
2022-10-08 10:56:08 +0200

    Connecting to the DB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}!`);
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });

f9b86dc488a0c9cd52068ee5a3757970d2369915
tornicke <torniqe@gmail.com>
2022-10-09 14:06:34 +0200

    Added put, update and delete functionalities

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -11,4 +11,57 @@ const addUser = async (req, res) => {
   }
 };
 
-module.exports = { addUser };
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    return res.status(200).json(allUsers);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Could not find users" });
+  }
+};
+
+const getSingleUser = async (req, res) => {
+  try {
+    const singleUser = await User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends");
+    return res.status(200).json(singleUser);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Could not find user" });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const updateUser = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+    return res.status(200).json(updateUser);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Could not update user" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const deleteUser = await User.findOneAndDelete({ _id: req.params.userId });
+
+    return res.status(200).json(deleteUser);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Could not delete user" });
+  }
+};
+
+module.exports = {
+  addUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+};
